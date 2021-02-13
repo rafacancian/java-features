b68a9471b7d8c5d621a0df7aed4a942e5df3904f
Rafael Cancian <cancian.rafa@gmail.com>
2021-02-13 17:37:40 +0000

    create project

---
+++ src/test/java/com/java/features/FeaturesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.features;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeaturesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
