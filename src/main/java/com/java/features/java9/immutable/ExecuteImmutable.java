42fa12aeb0db796ec56f1cc002959721887305be
Rafael Cancian <cancian.rafa@gmail.com>
2021-02-14 01:58:05 +0000

    java 9 - immutable collections

---
+++ src/main/java/com/java/features/java9/immutable/ExecuteImmutable.java
@@ -0,0 +1,18 @@
+package com.java.features.java9.immutable;
+
+import java.util.List;
+import java.util.Map;
+
+public class ExecuteImmutable {
+
+    public static void execute() {
+
+        List immutableList = List.of("One", "Two", "Three");
+        System.out.println("Immutable list: " + immutableList);
+
+        Map nonemptyImmutableMap = Map.of(1, "one", 2, "two", 3, "three");
+        System.out.println("Immutable map: " + nonemptyImmutableMap);
+
+
+    }
+}
