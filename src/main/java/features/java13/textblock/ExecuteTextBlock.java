1addad0ff69a6c68ad46c38884ad1a1972e46b82
Rafael Cancian <cancian.rafa@gmail.com>
2021-03-16 22:04:51 +0000

    java 13 textblock

---
+++ src/main/java/features/java13/textblock/ExecuteTextBlock.java
@@ -0,0 +1,25 @@
+package features.java13.textblock;
+
+public class ExecuteTextBlock {
+
+    public static void execute() {
+
+        String html = """
+                <html>
+                    <body>
+                        <p>Hello, World</p>
+                    </body>
+                </html>
+                        """;
+
+        String json = """
+                {
+                    "name":"mkyong",
+                    "age":38
+                }
+                """;
+
+    }
+
+
+}
